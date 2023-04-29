fe214f824513f3e5e4eae5d748eafc93fed3e4d7
Achmad <achmadsyahhidayat@gmail.com>
2023-04-29 22:29:29 +0700

    29Apr23, add method

--- src/PersonApp.java
+++ src/PersonApp.java
@@ -12,6 +12,13 @@ public class PersonApp {
         System.out.println(person1.name);
         System.out.println(person1.address);
         System.out.println(person1.country);
+
+        person1.sayHello("Andara");
+
+        Person person3;
+        person3 = new Person();
+        person3.name = "Anwar";
+        person3.sayHello("Tatang");
 //        System.out.println(person2);
 //        System.out.println(person3);
 
