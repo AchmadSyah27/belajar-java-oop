4e86cab9fccdbaa79d68a439aacaf312f54b2179
Achmad <achmadsyahhidayat@gmail.com>
2023-04-30 06:23:16 +0700

    29Apr23, add class, obj dan field

--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,18 @@
 class Person {
     String name;
+    String inputName;
     String address;
+    String inputAddress;
     final String country = "Indonesia";
 
-    void sayHello(String paramName){
-        System.out.println("Hello "+ paramName+", My Name is "+ name);
+    Person(String paramName, String paramAddress, String inputParamName, String inputParamAddress){
+        name = paramName;
+        inputName = inputParamName;
+        address = paramAddress;
+        inputAddress = inputParamAddress;
+    }
+
+    void sayHello(String paramName, String inputParamName, String paramAddress, String inputParamAddress){
+        System.out.println("Hello "+ inputParamName+", My Name is "+ paramName + " From "+ paramAddress);
     }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,9 +1,9 @@
 public class PersonApp {
     public static void main(String[] args) {
 
-        var person1 = new Person();
-        person1.name = "Achmad";
-        person1.address = "Depok";
+        var person1 = new Person( "", "","", "");
+//        person1.name = "Achmad"; line ini dapat dihapus, setelah penambahan parameter dalam constuctor
+//        person1.address = "Depok"; line ini dapat dihapus, setelah penambahan parameter dalam constuctor
 //        person1.country = "Amerika"; akan error karna jika sudah final tidak bisa diubah datanya.
 //        Person person2 = new Person();
 //        Person person3;
@@ -13,12 +13,15 @@ public class PersonApp {
         System.out.println(person1.address);
         System.out.println(person1.country);
 
-        person1.sayHello("Andara");
+        person1.sayHello("Achmad","Andara","Cimanggis","Bandung");
+
+        Person person2 = new Person("Budi", "Jakarta", "","");
 
         Person person3;
-        person3 = new Person();
-        person3.name = "Anwar";
-        person3.sayHello("Tatang");
+        person3 = new Person("", "","","");
+        person3.sayHello("Tony","Andrian","Cibubur","");
+//        person3.name = "Anwar"; line ini dapat dihapus, setelah penambahan parameter dalam constuctor
+//        person3.sayHello("Tatang");line ini dapat dihapus, setelah penambahan parameter dalam constuctor
 //        System.out.println(person2);
 //        System.out.println(person3);
 
